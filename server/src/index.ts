a6b023b18ca95aa9fae3d6876c9d6cc443e42526
ZeRo137 <angrymotion12@gmail.com>
2020-03-23 15:28:04 +0100

    commit c:

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 import * as WebSocket from "ws";
 import * as https from "https";
 import * as fs from "fs";
+import { types } from "util";
 
 enum EntityState {
     None = 0,
@@ -12,11 +13,28 @@ enum EntityState {
     Walk = 32,
     Idle = 64,
     Heal = 128,
-    Web = 256
+    Web = 132
+}
+
+enum EntityType {
+    PICKAXE,
+    SWORD,
+    HAMMER,
+    HAT,
+    WALL,
+    SPIKE,
+    DOOR,
+    FOOD,
+    MOB //etc..
 }
 
 class createWorld {
-    items = {HAND:{"id":14, "range": 50, "range2": 25, "damage": 5 }};
+    items = {STONE_SWORD:{id:0, range: 75, range2:25 , damage:19, odamage:2, itype:EntityType.SWORD },
+        HAND:{id:14, range: 50, range2: 25, damage: 5, odamge:1, itype:EntityType.PICKAXE },
+        WOOD_WALL:{id:16, sid: 0/* ah id is useless only for crafting needs spirite id atleast in modern version*/ hp: 1500, tier:0, itype:EntityType.WALL },
+        AME_SWORD:{id:50, range:80 ,range2:25, damage:30, odamage:10, itype:EntityType.SWORD},
+        DIA_HELMET:{id:45, coldProt:0, dmgProt:5, itype:EntityType.HAT},
+        };
     players = [];
     mapSize = {x: 10, y: 10 };
     chunks = new Array(this.mapSize.x);
@@ -33,6 +51,10 @@ class createWorld {
     
 }
 
+class Unity {
+
+}
+
 class getUtils {
     broadcastPacket(packet) {
         for (let player of world.players) {
@@ -45,21 +67,25 @@ class getUtils {
     } getLeaderboard() {
         let list = [];
         for (let player of world.players) {
-            list.push({ i: player.pid, n: player.nickname, p: player.score });
+            list.push({ i: player.pid, n: player.nick, p: player.score });
         }
         return list;
     } toHex(data) {
         return [data%256,Math.floor(data/ 256)];
     } angleToRad(ag) {
-        return ag / 180 * Math.PI;
+        return ag / 128 * Math.PI;
     } angleToCoords(ag) {
         ag = utils.angleToRad(ag);
-        return { "x": Math.sin(ag), "y": Math.cos(ag) };
+        return { "x": Math.cos(ag), "y": Math.sin(ag) };
     } distance(vector) {
         return (vector.x ** 2 + vector.y ** 2) ** 0.5;
+    } Uint8concat(arr1,arr2) {
+        let res = new Uint8Array(arr1.length+arr2.length);
+        res.set(arr1);
+        res.set(arr2,arr1.length);
+        return res
     }
 }
-
 
 interface Vector {
     x: number;
@@ -80,11 +106,11 @@ class Player {
     angle: number; 
     action: number = EntityState.None;
     chunk: Vector;
-    speed: number=100;
+    speed: number=300;
     tool = world.items.HAND;
-    helmet: number = 5;
+    hat = world.items.DIA_HELMET;
     bag: boolean;
-    score: number;
+    score: number = 0;
 
     //UPDATING
     moving: boolean;
@@ -104,7 +130,7 @@ class Player {
         this.nick = nick;
         this.token = token;
         this.ws = ws;
-        this.pos = { "x": Math.random() * 10000, "y": Math.random() * 10000 };
+        this.pos = { "x": 1 + Math.random() * 9998, "y": 1 + Math.random() * 9998 };
         this.movVector = { "x": 0, "y": 0 };
         for (let i = 1; i < 128; i++) {
             if (!world.players.find(e => e.pid == i)) {
@@ -112,11 +138,11 @@ class Player {
                 break;
             }
         }
+        world.players.push(this);
         this.ws.send(JSON.stringify([3, this.pid, 256, utils.getLeaderboard(), this.pos.x, this.pos.y, 256, 0, 0, null, [], 0]));
         this.chunk = { "x": Math.floor(this.pos.x / 1000), "y": Math.floor(this.pos.y / 1000) };
         this.sendInfos();
         world.chunks[this.chunk.x][this.chunk.y].push(this);
-        world.players.push(this);
         utils.broadcastPacket(JSON.stringify([2, this.pid, this.nick]));
         this.getInfos();
         this.updateLoop = setInterval(() => {
@@ -125,11 +151,11 @@ class Player {
                     this.pos.x += this.movVector.x/world.tickRate;
                     this.pos.y += this.movVector.y/world.tickRate;
                     // TODO physics here
-                    this.pos.x = Math.min(Math.max(0, this.pos.x), world.mapSize.x * 1000);
-                    this.pos.y = Math.min(Math.max(0, this.pos.y), world.mapSize.y * 1000);
+                    this.pos.x = Math.min(Math.max(0, this.pos.x), world.mapSize.x * 1000-1);
+                    this.pos.y = Math.min(Math.max(0, this.pos.y), world.mapSize.y * 1000-1);
                     let chunk = { "x": Math.floor(this.pos.x / 1000), "y": Math.floor(this.pos.y / 1000) };
                     if (this.chunk.x != chunk.x || chunk.y != this.chunk.y) {
-                        this.updateChunk(this.chunk);
+                        this.updateChunk(chunk);
                     }
                 }
                 this.sendInfos();
@@ -171,15 +197,11 @@ class Player {
     hit(ag) {
         this.angle = ag;
         this.attacking = true;
-        this.action |= EntityState.Attack;
+        this.hit2()
         if (!this.attackLoop) {
             this.attackLoop = setInterval(() => {
                 if (this.attacking) {
-                    this.action |= EntityState.Attack;
-                    let agCoords = utils.angleToCoords(this.angle);
-                    let center = { "x": agCoords.x * (this.tool.range + this.tool.range2) + this.pos.x, "y": agCoords.y * (this.tool.range + this.tool.range2) + this.pos.y };
-                    console.log(this.pos.x,this.pos.y,center)
-                    this.hit2(center, this.tool.range, this.tool.damage);
+                    this.hit2()
                 } else {
                     clearInterval(this.attackLoop);
                     this.attackLoop = null;
@@ -187,15 +209,16 @@ class Player {
             }, 500);
         }
     }
-    hit2(center, range, damage, odamage = 0) {
-        for (let x = -1; x <= 1; x++) {
-            if (this.chunk.x + x >= 0 && this.chunk.x + x < world.mapSize.x) {
-                for (let y = -1; y <= 1; y++) {
-                    if (this.chunk.y + y >= 0 && this.chunk.y + y < world.mapSize.y) {
-                        let players = world.chunks[x + this.chunk.x][y + this.chunk.y].filter(e => utils.distance({ x: center.x - e.pos.x, y: center.y - e.pos.y }) < range);
-                        for (let player of players) {player.damage(damage,this),player}
-                    }
-                }
+    hit2() {
+        this.action |= EntityState.Attack;
+        let agCoords = utils.angleToCoords(this.angle);
+        let center = { "x": agCoords.x * (this.tool.range + this.tool.range2) + this.pos.x, "y": agCoords.y * (this.tool.range + this.tool.range2) + this.pos.y };
+        let ymin = Math.max(-1 + this.chunk.y, 0), ymax = Math.min(2 + this.chunk.y, world.mapSize.y),
+            xmin = Math.max(-1 + this.chunk.x, 0), xmax = Math.min(2 + this.chunk.x, world.mapSize.x);
+        for (let x = xmin; x < xmax; x++) {
+            for (let y = ymin; y < ymax; y++) {
+                let players = world.chunks[x][y].filter(e => utils.distance({ x: center.x - e.pos.x, y: center.y - e.pos.y }) < this.tool.range);
+                for (let player of players) {player.damage(this.tool.damage,this),player}
             }
         }
     }
@@ -204,7 +227,6 @@ class Player {
     }
     damage(dmg,player) {
         this.health -= dmg;
-        console.log(dmg);
         this.action |= EntityState.Hurt;
         if (this.health < 0) {
             this.die();
@@ -242,13 +264,19 @@ class Player {
             }
         }
         this.sendInfos(true, list);
+        world.chunks[this.chunk.x][this.chunk.y] = world.chunks[this.chunk.x][this.chunk.y].filter(e=> e!= this);
         this.chunk = chunk;
+        world.chunks[this.chunk.x][this.chunk.y].push(this);
+        
     }
     sendInfos(visible = true, to: Player[] = null) {
         if (to !== null) {
+            let arr = new Uint8Array([0,0]);
             for (let player of to) {
+                arr = utils.Uint8concat(arr, player.infoPacket(visible));
                 player.ws.send(this.infoPacket(visible));
             }
+            this.ws.send(arr);
         } else {
             this.sendToRange(this.infoPacket(visible));
         }
@@ -257,8 +285,7 @@ class Player {
         if (visible) {
             let pos = { "x": utils.toHex(this.pos.x), "y": utils.toHex(this.pos.y) };
             let id = utils.toHex(this.id);
-            let infos = utils.toHex(this.tool.id+this.helmet*128+(this.bag ? 1 : 0)*16384);
-            console.log(this.action)
+            let infos = utils.toHex(this.tool.id+this.hat.id*128+(this.bag ? 1 : 0)*16384);
             return new Uint8Array([0, 0, this.pid, this.action, this.type, this.angle, pos.x[0], pos.x[1], pos.y[0], pos.y[1], id[0], id[1], infos[0], infos[1]]);
         } else {
             return new Uint8Array([0, 0, this.pid, 1, this.type, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
@@ -294,7 +321,6 @@ wss.on("connection", (ws, req) => {
 
     ws.binaryType = "arraybuffer";
     ws.on("message", (message) => {
-        //console.log(message);
         try {
             if (message instanceof ArrayBuffer) {
                 //nothing
@@ -317,8 +343,9 @@ wss.on("connection", (ws, req) => {
                         case 4:
                             player.hit(data[1]);
                             break;
-                        case 5:
+                        case 14:
                             player.stopHitting();
+                            break;
                     }
                 }
             }
